351886ded92f0f017e1dc8fa0c86f971b836f5e9
Cristian Vlaescu <cvl.escu@gmail.com>
2015-01-21 16:53:55 +0200

    added keys api

--- __init__.py
+++ __init__.py
@@ -33,21 +33,117 @@ from flask import Flask, request, Response, jsonify
 from help_json import *
 from geoprovdm import *
 
-app = Flask(__name__)
+import os
+from flask import  abort, g, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.httpauth import HTTPBasicAuth
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
 
-ENVIRON = 'PROD'
+# initialization
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users_db.sqlite'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+# extensions
+users_db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+#in this case, neo4j server and flask server are on the same machine, same SERVER_IP
+SERVER_IP="127.0.0.1"
+
 ENVIRON = os.environ.get('PROV_ENVIRON')
 if (ENVIRON == None):
    ENVIRON = 'PROD'
 
-
-db = GeoProvDM(ENVIRON, "http://localhost:7474/db/data/", False)
+db = GeoProvDM(ENVIRON, "http://%s:7474/db/data/"%SERVER_IP, False)
 
 @app.route("/provenance/test")
 def hello():
     return "Hello World!"
 
+class User(users_db.Model):
+    __tablename__ = 'users'
+    id = users_db.Column(users_db.Integer, primary_key=True)
+    username = users_db.Column(users_db.String(32), index=True)
+    password_hash = users_db.Column(users_db.String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None    # valid token, but expired
+        except BadSignature:
+            return None    # invalid token
+        user = User.query.get(data['id'])
+        return user
+
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+
+@app.route('/api/users', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)    # missing arguments
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400)    # existing user
+    user = User(username=username)
+    user.hash_password(password)
+    users_db.session.add(user)
+    users_db.session.commit()
+    return (jsonify({'username': user.username}), 201,
+            {'Location': url_for('get_user', id=user.id, _external=True)})
+
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
+
+
+@app.route('/api/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(600)
+    return jsonify({'token': token.decode('ascii'), 'duration': 600})
+
+
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({'data': 'Hello, %s!' % g.user.username})
+
 @app.route('/provenance/resource', methods=['POST'])
+@auth.login_required
 def create_resource_prov():
 
     # Atleast one entity should be present
@@ -106,6 +202,7 @@ def create_resource_prov():
 
 
 @app.route('/provenance/b/resource/<string:uuid>', methods=['GET'])
+@auth.login_required
 def get_resource_provenance(uuid):
  
   #obj = db.getSubgraph(uuid)
@@ -115,6 +212,7 @@ def get_resource_provenance(uuid):
   return Response(obj_json,mimetype='application/json',status=200)
 
 @app.route('/resource/<string:uuid>', methods=['DELETE'])
+@auth.login_required
 def delete_resource_provenance(uuid):
   uuid1 = uuid
   obj = db.deleteNodeByUuid(uuid1)
@@ -122,6 +220,7 @@ def delete_resource_provenance(uuid):
   return Response(obj_json,mimetype='application/json',status=200)
 
 @app.route('/provenance/b/resource/<string:uuid>/activity/<string:activityproperty>', methods=['GET'])
+@auth.login_required
 def get_resource_provenance_with_uuid_activityproperty(uuid,aprop):
   uuid1 = uuid
   aprop1 = aprop
@@ -130,6 +229,7 @@ def get_resource_provenance_with_uuid_activityproperty(uuid,aprop):
   return Response(obj_json,mimetype='application/json',status=200)
 
 @app.route('/provenance/<string:direction>/resource/<string:resourceproperty>', methods=['GET'])
+@auth.login_required
 def get_resource_provenance_with_resource_property(direction,rprop):
   direction1 = direction
   rprop1 = rprop
@@ -141,6 +241,7 @@ def get_resource_provenance_with_resource_property(direction,rprop):
   return Response(obj_json,mimetype='application/json',status=200)
 
 @app.route('/provenance/<string:direction>/resource/<string:resourceproperty>/activity/<string:activityproperty>', methods=['GET'])
+@auth.login_required
 def get_resource_provenance_with_resource_activity_property(direction, rprop, aprop):
   direction1 = direction
   rprop1 = rprop
@@ -153,6 +254,7 @@ def get_resource_provenance_with_resource_activity_property(direction, rprop, ap
   return Response(obj_json,mimetype='application/json',status=200)
 
 @app.route('/provenance/<string:direction>/activity/<string:activityproperty>/from/<string:datetime1>/to/<string:datetime2>', methods=['GET'])
+@auth.login_required
 def get_resource_provenance_with_activity_from_to(direction, aprop, t1,t2):
   direction1 = direction
   aprop1 = aprop
@@ -164,6 +266,8 @@ def get_resource_provenance_with_activity_from_to(direction, aprop, t1,t2):
      obj = db.getNodeGeneratedByActivityWithTimestamp(aprop1,t11,t21)
   obj_json = neo2json(obj)
   return Response(obj_json,mimetype='application/json',status=200)
-	
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    if not os.path.exists('users_db.sqlite'):
+        users_db.create_all()
+    app.run(host=SERVER_IP, port=5000, debug=True)
