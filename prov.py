07d3f8b9c89b8b67c751963e7c16b9430666073e
Cristian Vlaescu <cvl.escu@gmail.com>
2015-01-22 12:43:50 +0200

    added nodes and relatioships validations

--- prov.py
+++ prov.py
@@ -1,6 +1,7 @@
 import sys, os, datetime, decimal
 import json
 import networkx as nx
+from pprint import pprint
 
 class PROV:
     """ Main PROV class that cretaes a PROV graph from JSON object and validates it.
@@ -8,17 +9,67 @@ class PROV:
 
 
     def __init__(self, jsonobj):
+        def is_invalid_date(input_date_time):
+            #from time import mktime, strptime
+            from datetime import datetime
+
+            result = None
+
+            for format in ['%Y%m%d', '%Y%m%d.%H']:
+                try:
+                    result = datetime.strptime(input_date_time, format)
+                except:
+                    pass
+
+            if result is None:
+                # print 'Malformed date.'
+                return True
+            else:
+                # print 'Date is fine.'
+                return False
+
         provg  = nx.DiGraph()
 
         #add nodes; currently entities and activities
-        entities = jsonobj.get('entity') or []
+        entities = jsonobj.get('entity')
+        if entities is None:
+            return False, "there are no entities"
         activities = jsonobj.get('activity') or []
 
         for k in entities:
-            #print "adding " +str (k) + " having value =" + str(entities[k])
+            #print "adding entity " +str (k) + " having value ="
+            #pprint(entities[k])
+            if entities[k].get('foundry:UUID') is None:
+                ret = False, "there is no UUID in entity "+k
+            if entities[k].get('foundry:creationTime') is None:
+                ret = False, "there is no creationTime in entity "+k
+            if entities[k].get('foundry:version') is None:
+                ret = False, "there is no version in entity "+k
+            if is_invalid_date(entities[k]['foundry:creationTime']['$']):
+                ret = False, "invalid creationTime format in entity "+k
+
+            print entities[k]['foundry:creationTime']['$']
             provg.add_node(k, entities.get(k) )
         for k in activities:
+            print "adding activity " +str (k) + " having value ="
+            pprint(activities[k])
+            if activities[k].get('prov:type') is None:
+                ret =  False, "there is no type in activity "+k
+            if activities[k].get('prov:startTime') is None:
+                ret =  False, "there is no startTime in activity "+k
+            if activities[k].get('prov:endTime') is None:
+                ret =  False, "there is no endTime in activity "+k
+            if activities[k]['prov:endTime'] > activities[k]['prov:startTime']:
+                ret =  False, "startTime should be before endTime in activity "+k
+
+            if is_invalid_date(activities[k]['prov:startTime']):
+                ret = False, "invalid startTime format in activity "+k
+            if is_invalid_date(activities[k]['prov:endTime']):
+                ret = False, "invalid endTime format in activity "+k
+
+
             provg.add_node(k, activities.get(k) )
+
 
         #add relationships
         wasGeneratedBy = jsonobj.get('wasGeneratedBy') or []
@@ -26,20 +77,54 @@ class PROV:
         wasDerivedFrom = jsonobj.get('wasDerivedFrom') or []
 
         for k in wasGeneratedBy:
-            #print "adding edge " +str (k) + " having value =" + str(wasGeneratedBy[k])
-            provg.add_edge(wasGeneratedBy[k]['prov:entity'],wasGeneratedBy[k]['prov:activity'], dict(type=wasGeneratedBy, name=k) )
+            #The set of all entity nodes that were "wasGeneratedBy" an activity A have creation time between activity A's start_time and end_time
+            #print "adding wasGeneratedBy edge " +str (k) + " having value =" + str(wasGeneratedBy[k])
+            activity = wasGeneratedBy[k]['prov:activity']
+            entity = wasGeneratedBy[k]['prov:entity']
+            if activities[activity]['prov:endTime'] < entities[entity]['foundry:creationTime']:
+                ret =  False, "wasGeneratedBy error for "+k+":creationTime of entity "+entity+\
+                       " should be between startTime and endTime of activity "+activity
+            if activities[activity]['prov:startTime'] < entities[entity]['foundry:creationTime']:
+                ret =  False, "wasGeneratedBy error for "+k+":creationTime of entity "+entity+\
+                       " should be between startTime and endTime of activity "+activity
+            provg.add_edge(entity,activity, dict(type=wasGeneratedBy, name=k) )
         for k in used:
-            #print "adding edge " +str (k) + " having value =" + str(used[k])
-            provg.add_edge(used[k]['prov:activity'],used[k]['prov:entity'], dict(type=used, name=k) )
+            #The set of all entity nodes that were "used" by an activity A have creation time less than activity A's end_time.
+            #print "adding used edge " +str (k) + " having value =" + str(used[k])
+            activity = used[k]['prov:activity']
+            entity = used[k]['prov:entity']
+            if activities[activity]['prov:endTime'] < entities[entity]['foundry:creationTime']:
+                ret =  False, "used error for "+k+":creationTime of entity "+entity+\
+                       " should be before endTime of activity "+activity
+            provg.add_edge(activity,entity, dict(type=used, name=k) )
         for k in wasDerivedFrom:
-            #print "adding edge  " +str (k) + " having value =" + str(wasDerivedFrom[k])
-            provg.add_edge(wasDerivedFrom[k]['prov:usedEntity'],wasDerivedFrom[k]['prov:generatedEntity'], dict(type=wasDerivedFrom, name=k) )
+            #In the "wasDerivedBy" edge the source entity has a version number and creation time that is
+            # lower than destination entity version number and creation time, but have the same UUID.
+            entity_source = wasDerivedFrom[k]['prov:usedEntity']
+            entity_destination =wasDerivedFrom[k]['prov:generatedEntity']
+            print "source details: "
+            pprint(entities[entity_source])
+            print "destination details: "
+            pprint(entities[entity_destination])
+            if entities[entity_source]['foundry:UUID'] != entities[entity_destination]['foundry:UUID']:
+                ret =  False, "wasDerivedFrom error for "+k+":UUID of entity "+entity_source+\
+                       " should be the same as UUID of entity "+entity_destination
+            if entities[entity_source]['foundry:creationTime'] >= entities[entity_destination]['foundry:creationTime']:
+                ret =  False, "wasDerivedFrom error for "+k+":creationTime of entity "+entity_source+\
+                       " should be lower than creationTime of entity "+entity_destination
+            if entities[entity_source]['foundry:version'] >= entities[entity_destination]['foundry:version']:
+                ret =  False, "wasDerivedFrom error for "+k+":version of entity "+entity_source+\
+                       " should be lower than version of entity "+entity_destination
+
+            print "adding wasDerivedFrom edge  " +str (k) + " having value =" + str(wasDerivedFrom[k])
+            provg.add_edge(entity_source, entity_destination, dict(type=wasDerivedFrom, name=k) )
         print "-------------"
         print provg.nodes()
         print provg.edges()
 
-    def _validate(provgraph):
-       return True
+    def _validate(self, provgraph):
+
+        return True
 
 
 if __name__ == "__main__":
